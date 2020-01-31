45a6d5f3fad67156e66cf43dda55e62acc90f3dc
Gab melamed <gmelamed@WS-ACADBUS.EDU>
2020-01-31 09:47:15 -0800

    working on lab 130

--- Lab 130 Art Two/boid.js
+++ Lab 130 Art Two/boid.js
@@ -5,7 +5,8 @@ class Boid {//creates boid class
     this.loc = createVector(x, y);
     this.vel = createVector(vx, vy);
     this.acc = createVector(0, 0);
-    this.clr = color(255,105,180);
+    //this.clr = color(255,105,180);
+    this.clr = color(random(0, 255), random(0, 255), random(0, 255));
   }
 
   run(){//runs boid functions
@@ -18,15 +19,23 @@ class Boid {//creates boid class
     this.vel.limit(3);//limits the velocity
     this.vel.add(this.acc);//adds acceleration to velocity
     this.loc.add(this.vel);//adds velocity to location
+
+    // var distToCenter;
+    // distToCenter = this.loc.dist(mainBoid);//finds the distance from the ship(s) to the planet
+    //
+    // this.acc = p5.Vector.sub(mainBoid, this.loc);
+    // this.acc.normalize();
+    // this.acc.mult(0.1);
   }
 
   render(){//makes lines between the boids
     for(var i = boids.length - 1; i > 0; i--){
       if(this.loc.dist(boids[i].loc) < 200){
         stroke(this.clr);
-        line(this.loc.x, this.loc.y, boids[i].loc.x, boids[i].loc.y);
+        line(400, 400, boids[i].loc.x, boids[i].loc.y);
       }
     }
+    ellipse(this.loc.x, this.loc.y, 30, 30);
   }
 
   checkEdges(){//makes the boids bounce when they hit the edge of the canvas
