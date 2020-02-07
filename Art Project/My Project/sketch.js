b4df78024af8c5334f8c26f9522b80ace0f5f3f7
Gab melamed <gmelamed@WS-ACADBUS.EDU>
2020-02-07 09:49:20 -0800

    still working on art project

--- Art Project/My Project/sketch.js
+++ Art Project/My Project/sketch.js
@@ -1,23 +1,37 @@
 var squares = [];
+var triangles = [];
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background('white');
 }
 
 function draw() {
+	background(0, 0, 0, 5)
 	fill(random(255), random(0), random(255), random(10, 100));
 	noStroke();
 	ellipse(random(mouseX-80, mouseX+80), random(mouseY-80, mouseY+80), random(10, 40));
 	drawSquares();
+	drawTriangles();
 }
 
 function drawSquares(){
 	if(keyIsPressed === true && keyCode === UP_ARROW){
-		for(var i = 0; i < 100; i++){//creates the ship(s)
+		for(var i = 0; i < 5; i++){//creates the ship(s)
     	squares[i] = new Square(random(mouseX-80, mouseX+80), random(mouseY-80, mouseY+80));
   	}
 		for(var i = 0; i < squares.length; i++){//makes the ship(s) appear
     	squares[i].run();
+		}
+	}
+}
+
+function drawTriangles(){
+	if(keyIsPressed === true && keyCode === DOWN_ARROW){
+		for(var i = 0; i < 5; i++){//creates the ship(s)
+    	triangles[i] = new Triangle(random(mouseX-80, mouseX+80), random(mouseY-80, mouseY+80), random(mouseX-40, mouseX+40), random(mouseY-40, mouseY+40), random(mouseX-10, mouseX+10), random(mouseY-10, mouseY+10));
+  	}
+		for(var i = 0; i < triangles.length; i++){//makes the ship(s) appear
+    	triangles[i].run();
 		}
 	}
 }
--- Art Project/My Project/square.js
+++ Art Project/My Project/square.js
@@ -26,3 +26,30 @@ class Square {
   }
 
 }
+
+class Triangle {
+  //constructs values to pass into the buttons
+
+  constructor(x1, y1, x2, y2, x3, y3){
+    this.loc1 = createVector(x1, y1);
+    this.loc2 = createVector(x2, y2);
+    this.loc3 = createVector(x3, y3);
+    this.clr = color(random(255), random(255), random(255));
+  }
+
+
+  //displays the buttons on the screen
+
+  run() {
+    this.render();
+  }
+
+
+  //creates the button with the words in it
+
+  render() {
+    fill(this.clr);
+    triangle(this.loc1.x1, this.loc1.y1, this.loc2.x2, this.loc2.y2, this.loc3.x3, this.loc3.y3)
+  }
+
+}
