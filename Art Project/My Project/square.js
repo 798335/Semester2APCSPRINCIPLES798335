bd5b1e30871ce97ebd9bd69c2cb905ac78a33003
Gab melamed <gmelamed@WS-ACADBUS.EDU>
2020-02-12 08:44:57 -0800

    added comments and made the bubbles change color w/ seconds interval

--- Art Project/My Project/square.js
+++ Art Project/My Project/square.js
@@ -3,24 +3,17 @@
 
 class Square {
 
-  //constructs values to pass into the squares
-
-  constructor(x, y, clr){
+  constructor(x, y, clr){//constructs values to pass into the squares (loaction and color)
     this.loc = createVector(x, y);
     this.clr = clr;
   }
 
-  //displays the buttons on the screen
-
-  run() {
+  run() {//runs the methods in the Square class
     this.render();
   }
 
-  //creates the button with the words in it
-
-  render() {
+  render() {//creates the rectangles with random width and height and the chosen location in sketch
     fill(this.clr);
     rect(this.loc.x, this.loc.y, random(25, 100), random(25, 100))
   }
-
 }
